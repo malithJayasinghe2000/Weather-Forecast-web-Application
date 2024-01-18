d2f75b957e8acccd6b1b353a26660a3f5ed9c817
Malith Jayasinghe <malith2jayasinghe@gmail.com>
2024-01-19 00:19:04 +0530

    all done

---
+++ src/api.js
@@ -0,0 +1,15 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '33bd2fc6damshdddb2f28e20e448p1081bfjsn725440df653b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = '2a97104fa1035ed13a67fd3307f7f996';
+
